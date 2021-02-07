6d390ace539d0d0b87c2016c1a928eb2c425dc5f
Linus Hofmann <strajuck@gmail.com>
2021-02-07 11:44:32 +0100

    PHP-Impl

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer class="mobile-footer">
+    <div class="container d-block d-sm-none" style="padding-top: 0; margin-top: 0;">
+    
+    <div>
+			<a class="footer-link1" href="main.html" sryle="border-left:1px solid;">Home</a>
+			<a class="footer-link1" href="datenschutz.html" d>Datenschutz</a>
+			<a class="footer-link1" href="Impressum.html">Impressum</a>
+			
+			
+        <div class="mobile" style="color: darkgrey; margin-top: 2vw">Copyright © 2020 BASYS mbH</div>
+		</div></div></footer>
+    <footer class="page-footer  d-none d-sm-block">
+ <!-- Desktop -->
+  <div class="container ">
+    <section><div><p class="ueber">Kontakt</p></div></section>    
+    <div class="row">
+      <div class="col-lg-8 col-md-8 col-sm-12">
+          <p>BASYS Beratungsgesellschaft für angewandte Systemforschung mbH<br>
+Reisinigerstraße 25, 86159 Augsburg<br>
+info@basys.de<br>
++49821 257940<br>
+</p>
+    </div>
+    <div class="col-lg-4 col-md-4 col-sm-12">
+      <div class="footer-copyright text-center"><ul class="footer-link">
+  <li class="footer-nav-item"><a href="Impressum.html" style=" padding-right: 1vw; color: #00AA98">IMPRESSUM</a></li>
+  <li class="footer-nav-item"><a href="datenschutz.html" style=" padding-right: 1vw; color: #00AA98">DATENSCHUTZ</a></li>
+</ul></div>
+    </div>
+  </div>
+
+  <div class="footer-copyright text-center">© 2020 Copyright by BASYS</div>
+</div> 
+</footer>
